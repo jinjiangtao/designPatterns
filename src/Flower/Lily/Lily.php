f5ff6652aa86ef9150ba1c26583ea7fc570cca08
jinjiangtao <840129243@qq.com>
2018-10-28 17:36:17 +0800

    init

---
+++ src/Flower/Lily/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiangtao
+ * Date: 18/10/28
+ * Time: 下午5:08
+ */
+namespace Flower\Lily;
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
+
