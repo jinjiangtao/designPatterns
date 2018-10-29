f5363c4644ec7615675ab034ea523d2a6b14a9aa
jinjiangtao <840129243@qq.com>
2018-10-29 09:13:36 +0800

    init && dev package

---
+++ src/DesignPatterns/SimpleFactory/SimpleFactory.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiangtao
+ * Date: 18/10/29
+ * Time: 上午9:00
+ */
+namespace DesignPatterns\SimpleFactory;
+
+class SimpleFactory
+{
+    public function test()
+    {
+        echo "simple factory test";
+    }
+}
+
+
---
+++ src/DesignPatterns/Singleton/Singleton.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiangtao
+ * Date: 18/10/29
+ * Time: 上午9:01
+ */
+namespace DesignPatterns\Singleton;
+
+class Singleton
+{
+    public function testSinleton()
+    {
+        echo "test Sinleton";
+    }
+}
